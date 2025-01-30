b93800b734f1124ef0851a6d359dc49e6ee84250
Murilo Vieira <murilo12super@gmail.com>
2025-01-29 21:08:39 -0300

    Criado tela de login

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform } from 'react-native'
 import React from 'react'
+import { 
+  BackGround, 
+  Container, 
+  Logo, 
+  AreaInput, 
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText
+ } from './styles';
+import { StackNavigationProp } from '@react-navigation/stack';
+import SignListParam from '../../types/SignListParam';
+import { useNavigation } from '@react-navigation/native';
+
+type SignNavigationProp = StackNavigationProp<SignListParam, "SignIn">;
 
 const SignIn: React.FC = () => {
+
+  const navigation = useNavigation<SignNavigationProp>();
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <BackGround>
+      <Container
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        enabled
+      >
+        <Logo source={require('../../assets/Logo.png')}/>
+
+        <AreaInput>
+          <Input
+            placeholder="Digite seu e-mail"
+          />
+        </AreaInput>
+        <AreaInput>
+          <Input
+            placeholder="Digite sua senha"
+          />
+        </AreaInput>
+
+        <SubmitButton
+          activeOpacity={0.8}
+        >
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link
+          onPress={navigation.navigate('SignUp')}
+        >
+          <LinkText>Criar uma conta</LinkText>
+        </Link>
+      </Container>
+    </BackGround>
   )
 }
 
 export default SignIn
-
-const styles = StyleSheet.create({})
